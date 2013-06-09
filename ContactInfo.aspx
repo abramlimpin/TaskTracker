89d48e7b44874baabaa122fc5f8b36547fc8d442
Abram John Limpin <abramjohn.limpin@benilde.edu.ph>
2013-06-09 11:00:44 +0800

    Added ContactInfo.aspx

--- ContactInfo.aspx
+++ ContactInfo.aspx
@@ -6,5 +6,70 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="dashboard" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="content" Runat="Server">
+<div class="one-half">
+    <asp:Panel ID="pnlAdd" runat="server" Visible="false">
+        <div class="note-box note-success hide-me">
+            <p>Contact added.</p>
+        </div>
+    </asp:Panel>
+    <form id="Form1" runat="server" class="form">
+        <fieldset>
+            <div class="widget">
+                <div class="title"><img src="http://localhost:12345/TaskTracker/images/icons/light/user-2.png" alt="" class="title-icon" />
+                <h6>Add a Contact</h6></div>
+                <div class="form-row">
+                    <label>Name</label>
+                    <div class="form-right">
+                    <span class="one-half">
+                        <asp:TextBox ID="txtFN" runat="server" MaxLength="50"
+                            placeholder="First Name" />
+                        </span>
+                    <span class="one-half">
+                        <asp:TextBox ID="txtLN" runat="server" MaxLength="50"
+                            placeholder="Last Name" />
+                    </span></div>
+                    <div class="clear"></div>
+                </div>
+                <div class="form-row">
+                    <label>Nickname</label>
+                    <div class="form-right"><span class="one-half">
+                    <asp:TextBox ID="txtNickname" runat="server" MaxLength="50" />
+                    </span></div>
+                    <div class="clear"></div>
+                </div>
+                <div class="form-row">
+                    <label>Contact</label>
+                    <div class="form-right">
+                    <span class="one-half">
+                        <asp:TextBox ID="txtEmail" runat="server" MaxLength="50"
+                            placeholder="Email Address" />
+                    </span>
+                    <span class="one-half">
+                        <asp:TextBox ID="txtMobile" runat="server" MaxLength="50"
+                            placeholder="Mobile No." />
+                    </span>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="form-row">
+                    <label for="labelFor">Group</label>
+                    <div class="form-right">
+                    <asp:Panel ID="pnlGroup" runat="server">
+                    <span class="one-half">
+                        <asp:DropDownList ID="ddlGroup" runat="server" />
+                    </span>
+                    </asp:Panel>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+
+                <div class="form-row">
+                    <asp:LinkButton ID="btnUpdate" runat="server" class="wide-button blue-button" 
+                        Text="Update Contact" Width="200" OnClick="btnUpdate_Click" />
+                </div>
+            </div>
+        </fieldset>
+    </form>
+    </div>
 </asp:Content>
 
--- ContactInfo.aspx.cs
+++ ContactInfo.aspx.cs
@@ -5,10 +5,99 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data;
+using System.Data.SqlClient;
+
 public partial class ContactInfo : System.Web.UI.Page
 {
+    SqlConnection con = new SqlConnection(Helper.GetConnectionString());
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["userid"] != null &&
+            Request.QueryString["ID"] != null)
+        {
+            int contactID = 0;
+            bool isValid = int.TryParse(Request.QueryString["ID"].ToString(), out contactID);
 
+            if (isValid)
+            {
+                if (!IsPostBack)
+                {
+                    GetGroup();
+                    GetContactInfo(contactID);
+                }
+            }
+        }
+        else
+            Response.Redirect("Login.aspx");
+    }
+
+    void GetContactInfo(int ID)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "SELECT tblContact.FirstName, tblContact.LastName, tblContact.Nickname, " +
+            "tblContact.Mobile, tblContact.Email, tblGroup.GroupID FROM tblContact " +
+            "INNER JOIN tblGroup ON tblContact.GroupID = tblGroup.GroupID " +
+            "WHERE tblGroup.UserID = @UserID AND tblContact.GroupID = @GroupID";
+        cmd.Parameters.Add("@GroupID", SqlDbType.Int).Value = ID;
+        cmd.Parameters.Add("@UserID", SqlDbType.NVarChar).Value = Session["userid"].ToString();
+        SqlDataReader data = cmd.ExecuteReader();
+        if (data.HasRows)
+        {
+            while (data.Read())
+            {
+                txtFN.Text = data["FirstName"].ToString();
+                txtLN.Text = data["LastName"].ToString();
+                txtNickname.Text = data["Nickname"].ToString();
+                txtEmail.Text = data["Email"].ToString();
+                txtMobile.Text = data["Mobile"].ToString();
+                ddlGroup.SelectedValue = data["GroupID"].ToString();
+            }
+            con.Close();
+        }
+        else
+        {
+            con.Close();
+            Response.Redirect("Contacts.aspx");
+        }
+    }
+
+    void GetGroup()
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "SELECT GroupID, GroupName FROM tblGroup WHERE UserID = @UserID";
+        cmd.Parameters.Add("@UserID", SqlDbType.NVarChar).Value = Session["userid"].ToString();
+        SqlDataReader data = cmd.ExecuteReader();
+        ddlGroup.DataSource = data;
+        ddlGroup.DataTextField = "GroupName";
+        ddlGroup.DataValueField = "GroupID";
+        ddlGroup.DataBind();
+        con.Close();
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "UPDATE tblContact SET FirstName = @FirstName, LastName = @LastName, " +
+            "Nickname = @Nickname, Email = @Email, Mobile = @Mobile, GroupID = @GroupID " +
+            "WHERE ContactID = @ContactID";
+        cmd.Parameters.Add("@FirstName", SqlDbType.NVarChar).Value = txtFN.Text;
+        cmd.Parameters.Add("@LastName", SqlDbType.NVarChar).Value = txtLN.Text;
+        cmd.Parameters.Add("@Nickname", SqlDbType.NVarChar).Value = txtNickname.Text;
+        cmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = txtEmail.Text;
+        cmd.Parameters.Add("@Mobile", SqlDbType.NVarChar).Value = txtMobile.Text;
+        cmd.Parameters.Add("@GroupID", SqlDbType.Int).Value = ddlGroup.SelectedValue;
+        cmd.Parameters.Add("@ContactID", SqlDbType.Int).Value = Request.QueryString["ID"].ToString();
+        cmd.ExecuteNonQuery();
+        con.Close();
+        Session["contact_edit"] = "yes";
+        Response.Redirect("Contacts.aspx");
     }
 }
